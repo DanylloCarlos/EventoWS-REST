939d330f7111d23baaa191e8c7a6376a7c11eaba
danyllocarlos <danyllo.rsilva@gmail.com>
2018-08-04 14:17:48 -0300

    source files

---
+++ src/dao/ClienteDAO.java
@@ -0,0 +1,120 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelo.Clientes;
+import util.ConexaoBD;
+
+public class ClienteDAO {
+	private Connection c;
+	private PreparedStatement pstm;
+	private ResultSet rs;
+	private String sql;
+	ArrayList<Clientes> listaDeClientes;
+	
+	public ClienteDAO(){
+		try {
+			c = ConexaoBD.novaConexao();
+		
+		}catch(SQLException | ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void cadastrarCliente(String nome, String cpf, int idEvento){
+		
+		String sql = "Insert into Clientes (nomeCliente, cpf, Eventos_idEvento) values (?, ?, ?)";
+		
+		try {
+			pstm = c.prepareStatement(sql);
+			
+			pstm.setString(1, nome);
+			pstm.setString(2, cpf);
+			pstm.setInt(3, idEvento);
+			
+			pstm.executeUpdate();
+			
+			pstm.close();
+			c.close();
+			
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
+			
+	}
+	
+	public void removerCliente(Clientes cli){
+	
+		String sql = "Delete from Clientes c where c.idCliente = ?";
+		
+		try {
+			pstm = c.prepareStatement(sql);
+			pstm.setInt(1, cli.getIdCliente());
+			pstm.executeQuery();
+			
+			pstm.close();
+			c.close();
+			
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
+		
+	}
+	
+	public Clientes buscarCliente(String cpf){
+		Clientes cli = new Clientes();
+		
+		String sql = "Select * from Clientes c where c.cpf= ?";
+		
+		try {
+			pstm = c.prepareStatement(sql);
+			pstm.setString(1, cpf);
+			rs = pstm.getResultSet();
+			
+			pstm.close();
+			c.close();
+			
+			while(rs.next()) {
+				cli.setIdCliente(rs.getInt("idCliente"));
+				cli.setNomeCliente(rs.getString("nomeCliente"));
+				cli.setCpf(rs.getString("cpf"));
+				
+			}
+			
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
+		
+		return cli;
+	}
+	
+	public ArrayList<Clientes> listarClientes(){
+		listaDeClientes = new ArrayList<Clientes>();
+		sql = "Select * from Clientes";
+		
+		try {
+			pstm = c.prepareStatement(sql);
+			rs = pstm.executeQuery();
+			
+			while(rs.next()){
+				Clientes clientes = new Clientes();
+				clientes.setIdCliente(rs.getInt("idCliente"));
+				clientes.setNomeCliente(rs.getString("nomeCliente"));
+				clientes.setCpf(rs.getString("cpf"));
+				listaDeClientes.add(clientes);
+			}
+			pstm.close();
+			c.close();
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}			
+		return listaDeClientes;
+		
+	}
+}
---
+++ src/dao/EventoDAO.java
@@ -0,0 +1,132 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelo.Clientes;
+import modelo.Eventos;
+import util.ConexaoBD;
+
+public class EventoDAO {
+	private Connection c;
+	private PreparedStatement pstm;
+	private ResultSet rs;
+	private String sql;
+	private ArrayList<Clientes> listaClientesPorEvento;
+	
+	public EventoDAO() {
+		
+		try {
+			c = ConexaoBD.novaConexao();
+		
+		}catch(SQLException | ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void cadastrarEvento(String nomeDoEvento, int qtdIngressos, int idCliente){
+		
+		sql = "Insert into Eventos(nomeEvento, qtdIngressos)"
+				+ " values (?, ?)";
+		
+		try {
+			
+			pstm = c.prepareStatement(sql);
+			pstm.setString(1, nomeDoEvento);
+			pstm.setInt(2, qtdIngressos);
+			
+			pstm.executeUpdate();
+			
+			pstm.close();
+			c.close();
+			
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
+	}
+	
+	public void removerEvento(Eventos e){
+		
+		String sql = "Delete from Eventos e where e.IdEvento = ?";
+		
+		try {
+			pstm = c.prepareStatement(sql);
+			pstm.setInt(1, e.getIdEvento());
+			
+			pstm.execute();
+			
+			pstm.close();
+			c.close();
+			
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
+		
+		
+	}
+	
+	public Eventos buscarEvento(int idEvento){
+		
+		Eventos e = new Eventos();
+		
+		String sql = "Select * from Eventos e where e.idEvento =?";
+		
+		try {
+			pstm = c.prepareStatement(sql);
+			pstm.setInt(1, idEvento);
+			rs = pstm.executeQuery();
+			
+			pstm.close();
+			c.close();
+			
+			while(rs.next()) {
+				e.setIdEvento(rs.getInt("idEvento"));
+				e.setNomeEvento(rs.getString("nomeEvento"));
+				e.setClientes_idCliente(rs.getInt("Clientes_idCliente"));
+				e.setIngressos_idIngresso(rs.getInt("Ingressos_idIngresso"));
+			}
+			
+			return e;
+		
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public ArrayList<Clientes> listarClientesPorEvento(int codigoDoEvento) {
+		listaClientesPorEvento = new ArrayList<>();
+		
+		sql = "Select nomeCliente, cpf from Clientes c, Eventos e "
+				+ "Where c.Eventos_idEvento = e.idEvento and e.idEvento = ?";
+		
+			try {
+				pstm = c.prepareStatement(sql);
+				pstm.setInt(1, codigoDoEvento);
+				rs = pstm.executeQuery();
+				
+				while(rs.next()){
+
+					Clientes cli = new Clientes();
+					
+					cli.setNomeCliente(rs.getString("nomeCliente"));
+					cli.setCpf(rs.getString("cpf"));
+					
+					listaClientesPorEvento.add(cli);
+				}
+				
+				pstm.close();
+				c.close();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return listaClientesPorEvento;
+		}	
+}
